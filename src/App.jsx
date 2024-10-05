02a499392865ac1fb641d073bded654a774dfafb
thanhquang2004 <quangthanh7204@gmail.com>
2024-10-05 16:34:02 +0700

    Add theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 
 function App() {
 
-  return <Button variant="contained">Hello world</Button>;
+  return <>
+    <Button variant="contained">Hello world</Button>
+    <Typography variant="h1">Hello world</Typography>
+  </>;
 }
 
 export default App
