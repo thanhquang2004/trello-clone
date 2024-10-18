92793bffcbbc01ad51e3ed28c59db668f5985346
thanhquang2004 <quangthanh7204@gmail.com>
2024-10-18 16:45:07 +0700

    Create basic layout and change site name

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,10 @@ import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 
 // Create a theme instance.
 const theme = extendTheme({
+  trelloCustomizations: {
+    appBarHeight: "48px",
+    boardBarHeight: "58px",
+  },
   colorSchemes: {
     light: {
       palette: {
