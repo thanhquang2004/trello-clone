a626cef67d61f08bc07b40666a0b6f67d5c57b61
thanhquang2004 <quangthanh7204@gmail.com>
2024-11-26 21:42:09 +0700

    Add drag and drop card

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -3,7 +3,9 @@ import { Box } from "@mui/material";
 import ListColumns from "./ListColumns/ListColumns";
 import { mapOrder } from "~/utils/sorts";
 import {
+  defaultDropAnimationSideEffects,
   DndContext,
+  DragOverlay,
   MouseSensor,
   // PointerSensor,
   TouchSensor,
@@ -12,6 +14,13 @@ import {
 } from "@dnd-kit/core";
 import { useEffect, useState } from "react";
 import { arrayMove } from "@dnd-kit/sortable";
+import Column from "./ListColumns/Column/Column";
+import Card from "./ListColumns/Column/ListCards/Card/Card";
+
+const ACTIVE_DRAG_ITEM_TYPE = {
+  COLUMN: "COLUMN",
+  CARD: "CARD",
+};
 
 function BoardContent({ board }) {
   // const pointerSensor = useSensor(PointerSensor, {
@@ -31,13 +40,31 @@ function BoardContent({ board }) {
     },
   });
   // const sensors = useSensors(pointerSensor);
-  const sensors = useSensors( mouseSensor, touchSensor);
+  const sensors = useSensors(mouseSensor, touchSensor);
 
   const [orderedColumns, setOrderedColumns] = useState([]);
+
+  //Cung mot thoi diem chi co 1 item duoc drag {column hoac card}
+  const [activeDragItemId, setActiveDragItemId] = useState(null);
+
+  const [activeDragItemType, setActiveDragItemType] = useState(null);
+
+  const [activeDragItemData, setActiveDragItemData] = useState(null);
 
   useEffect(() => {
     setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, "_id"));
   }, [board]);
+
+  const handleDragStart = (event) => {
+    console.log(event);
+    setActiveDragItemId(event?.active?.id);
+    setActiveDragItemType(
+      event?.active?.data?.current?.columnId
+        ? ACTIVE_DRAG_ITEM_TYPE.CARD
+        : ACTIVE_DRAG_ITEM_TYPE.COLUMN
+    );
+    setActiveDragItemData(event?.active?.data?.current);
+  };
 
   const handleDragEnd = (event) => {
     console.log(event);
@@ -56,11 +83,28 @@ function BoardContent({ board }) {
       const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex);
       // const dndColumnOrderIds = dndOrderedColumns.map((c) => c._id);
       setOrderedColumns(dndOrderedColumns);
+      setActiveDragItemId(null);
+      setActiveDragItemType(null);
+      setActiveDragItemData(null);
     }
   };
 
+  const dropAnimation = {
+    sideEffects: defaultDropAnimationSideEffects({
+      styles: {
+        active: {
+          opacity: 0.5,
+        },
+      },
+    }),
+  };
+
   return (
-    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+    <DndContext
+      onDragEnd={handleDragEnd}
+      onDragStart={handleDragStart}
+      sensors={sensors}
+    >
       <Box
         sx={{
           width: "100%",
@@ -73,6 +117,15 @@ function BoardContent({ board }) {
         }}
       >
         <ListColumns columns={orderedColumns} />
+        <DragOverlay dropAnimation={dropAnimation}>
+          {!activeDragItemType && null}
+          {activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.COLUMN && (
+            <Column key={activeDragItemId} column={activeDragItemData} />
+          )}
+          {activeDragItemType === ACTIVE_DRAG_ITEM_TYPE.CARD && (
+            <Card key={activeDragItemId} card={activeDragItemData} />
+          )}
+        </DragOverlay>
       </Box>
     </DndContext>
   );
--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -12,8 +12,20 @@ import {
 } from "@mui/material";
 
 import { Card as MuiCard } from "@mui/material";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 function Card({ card }) {
+  const { attributes, listeners, setNodeRef, transform, transition, isDragging } =
+    useSortable({ id: card?._id, data: { ...card } });
+
+  const dndKitCardStyle = {
+    // touchAction: "none",
+    transform: CSS.Translate.toString(transform),
+    transition,
+    opacity: isDragging ? 0.5 : undefined,
+  };
+
   const shouldShowCardACtions = () => {
     return (
       !!card?.members?.length ||
@@ -24,6 +36,10 @@ function Card({ card }) {
 
   return (
     <MuiCard
+    ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={dndKitCardStyle}
       sx={{
         cursor: "pointer",
         boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
